bc4a39bbf58e8329b01c98567eb6fc33f4d71e5b
lachimolala2628 <aayushkumar1228@gmail.com>
2024-05-06 02:28:29 +0530

    change UI color

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Flex } from '@chakra-ui/react'
 const App = () => {
   return (
     <>
-      <Container backgroundImage="url('https://i.pinimg.com/736x/2b/ac/38/2bac38a35adb674c9efce30a33597d8f.jpg')" backgroundPosition="center" backgroundSize='cover' backgroundRepeat="no-repeat" maxW={'Container.3xl'} height={'100vh'} p={'0'}>
+      <Container bg="#322C2B" maxW={'Container.3xl'} height={'100vh'} p={'0'}>
         <Flex height={'full'}>
           <Box height={'full'} flex={5} w={['20%', '30%', '20%', '50%', '60%']}>
             <Main />
